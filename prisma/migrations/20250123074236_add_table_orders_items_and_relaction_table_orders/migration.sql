2a13e2fe51957725ff11dae79afd9cab860c13d6
victor martinez <vitico841@gmail.com>
2025-01-23 03:45:16 -0400

    Agregar tabla order_items y establecer relación con la tabla order

---
+++ prisma/migrations/20250123074236_add_table_orders_items_and_relaction_table_orders/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "order_items" (
+    "id" UUID NOT NULL,
+    "order_id" UUID NOT NULL,
+    "product_id" UUID NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "order_items_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "order_items" ADD CONSTRAINT "order_items_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
